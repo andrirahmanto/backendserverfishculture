b3ecf930b251bc09f14cca5dbb4904b17abc1ae5
Andri Rahmanto <rahmanto660@gmail.com>
2022-03-29 23:48:56 +0700

    feat: add pond crud

---
+++ resources/pond.py
@@ -0,0 +1,32 @@
+from flask import Response, request
+from database.models import Pond
+from flask_restful import Resource
+import datetime
+
+
+class PondsApi(Resource):
+    def get(self):
+        ponds = Pond.objects().to_json()
+        return Response(ponds, mimetype="application/json", status=200)
+
+    def post(self):
+        body = request.get_json()
+        pond = Pond(**body).save()
+        id = pond.id
+        return {'id': str(id)}, 200
+
+
+class PondApi(Resource):
+    def put(self, id):
+        body = request.get_json()
+        body['updated_at'] = datetime.datetime.utcnow()
+        Pond.objects.get(id=id).update(**body)
+        return '', 200
+
+    def delete(self, id):
+        pond = Pond.objects.get(id=id).delete()
+        return '', 200
+
+    def get(self, id):
+        pond = Pond.objects.get(id=id).to_json()
+        return Response(pond, mimetype="application/json", status=200)
