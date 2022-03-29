3aa19e8c099692df19cdf6d672686be7a1178176
Andri Rahmanto <rahmanto660@gmail.com>
2022-03-29 23:49:58 +0700

    update: add new path for api

--- resources/routes.py
+++ resources/routes.py
@@ -1,6 +1,18 @@
 from .logfeeding import LogFeedingsApi, LogFeedingApi
+from .pond import PondsApi, PondApi
+from .typefeed import TypeFeedsApi, TypeFeedApi
 
 
 def initialize_routes(api):
+
+    # pond
+    api.add_resource(PondsApi, '/api/ponds')
+    api.add_resource(PondApi, '/api/ponds/<id>')
+
+    # typefeed
+    api.add_resource(TypeFeedsApi, '/api/typefeeds')
+    api.add_resource(TypeFeedApi, '/api/typefeeds/<id>')
+
+    # logfeeding
     api.add_resource(LogFeedingsApi, '/api/logfeedings')
     api.add_resource(LogFeedingApi, '/api/logfeedings/<id>')
