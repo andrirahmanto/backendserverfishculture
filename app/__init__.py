b3ae1a0268b321469ba4fc50faa8ea50be2c8aa8
andrirahmanto <rahmanto660@gmail.com>
2022-04-11 17:26:18 +0700

    feat: add view table feedhistory

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from .database.db import initialize_db
 from flask_restful import Api
+from app.database.models import FeedHistory
 from .resources.routes import initialize_routes
+from bson.json_util import dumps
+import json
+import datetime
 
 
 def create_app(test_config=None):
@@ -17,5 +21,22 @@ def create_app(test_config=None):
     initialize_db(app)
     initialize_routes(api)
 
-    # app.run()
+
+    @app.route('/')
+    def home():
+        # data = FeedHistory.objects().to_json()
+        # # feedhistory = feedhistorys[0].to_json()
+        # data = json.loads(data)
+        # date = data[0]["feed_history_time"]["$date"]
+        # date_up = data[0]["feed_history_time"]["$date"]
+        # print (type(date))
+        datas = FeedHistory.objects()
+        datas_list = []
+        for data in datas:
+            data_dict = data.to_mongo().to_dict()
+            data_dict['created_at'] = data.created_at.isoformat()
+            datas_list.append(data_dict)
+        data = datas_list    
+        return render_template('home.html', name = 'Andri', feedhistorys = data)
+
     return app
