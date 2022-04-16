ba1a75ec802f381b48f523d72c379ffa2b3e1900
Andri Rahmanto <rahmanto660@gmail.com>
2022-04-16 22:49:42 +0700

    feat: remodel document database

--- app/database/models.py
+++ app/database/models.py
@@ -4,7 +4,8 @@ from bson import json_util
 
 
 class Pond(db.Document):
-    name = db.StringField(required=True)
+    id_int = db.IntField(required=True)
+    alias = db.StringField(required=True)
     location = db.StringField(required=True)
     created_at = db.DateTimeField(default=datetime.datetime.utcnow)
     updated_at = db.DateTimeField(default=datetime.datetime.utcnow)
@@ -12,7 +13,10 @@ class Pond(db.Document):
 
 class FeedType(db.Document):
     name = db.StringField(required=True)
-    desc = db.StringField(required=True)
+    feed_type = db.StringField(required=True)
+    protein = db.IntField(required=True)
+    carbohydrate = db.IntField(required=True)
+    desc = db.StringField()
     created_at = db.DateTimeField(default=datetime.datetime.utcnow)
     updated_at = db.DateTimeField(default=datetime.datetime.utcnow)
 
@@ -21,21 +25,6 @@ class FeedHistory(db.Document):
     pond_id = db.ReferenceField(Pond, required=True)
     feed_type_id = db.ReferenceField(FeedType, required=True)
     feed_dose = db.IntField(required=True)
-    feed_desc = db.StringField(default="")
     feed_history_time = db.DateTimeField(default=datetime.datetime.utcnow)
     created_at = db.DateTimeField(default=datetime.datetime.utcnow)
     updated_at = db.DateTimeField(default=datetime.datetime.utcnow)
-
-    # def to_json(self):
-    #     data = self.to_mongo()  # get the pymongo representation of the document
-    #     data["pond"] = {
-    #         "_id": self.pond_id.id,
-    #         "name": self.pond_id.name,
-    #         "location": self.pond_id.location
-    #     }
-    #     data["type_feed"] = {
-    #         "_id": self.type_feed_id.id,
-    #         "name": self.type_feed_id.name,
-    #         "desc": self.type_feed_id.desc
-    #     }
-    #     return json_util.dumps(data)
