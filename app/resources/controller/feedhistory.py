4891f1f7b13fe441c14d8fca155eed2a330f24e9
Andri Rahmanto <rahmanto660@gmail.com>
2022-05-15 10:22:54 +0700

    feat: add get feedhistory today by pond

--- app/resources/controller/feedhistory.py
+++ app/resources/controller/feedhistory.py
@@ -1,8 +1,10 @@
 from flask import Response, request
 from app.database.models import FeedHistory, Pond, FeedType
 from flask_restful import Resource
+from app.database.db import db
 import datetime
 import json
+from bson.json_util import dumps
 
 
 class FeedHistorysApi(Resource):
@@ -99,4 +101,69 @@ class FeedHistoryApi(Resource):
         feedhistory["pond"] = pond
         feedhistory["feed_type"] = feedtype
         response = json.dumps(feedhistory, default=str)
+        return Response(response, mimetype="application/json", status=200)
+
+
+class Test(Resource):
+    def get(self):
+        pipline = [
+            {'$lookup': {
+                'from': 'pond',
+                'localField': 'pond_id',
+                'foreignField': '_id',
+                'as': 'Matrix'
+            }
+            }
+        ]
+        feedHistorys = FeedHistory.objects().aggregate(pipline)
+        response = []
+        for feedHistory in feedHistorys:
+            response.append(feedHistory)
+        print(type(response))
+        response = json.dumps(response, default=str)
+        print(type(response))
+        return Response(response, mimetype="application/json", status=200)
+
+
+class Test2(Resource):
+    def get(self):
+        start = datetime.datetime.today().replace(
+            hour=0, minute=0, second=0, microsecond=0)
+        start = start - datetime.timedelta(hours=24)
+        start = start.strftime('%Y-%m-%d')
+        print(start)
+        pipline = [
+            {'$lookup': {
+                'from': 'feed_history',
+                'let': {"pondid": "$_id"},
+                'pipeline': [
+                    {'$match': {'$expr': {'$and': [
+                        {'$eq': ['$pond_id', '$$pondid']},
+                        {'$eq': [start, {'$dateToString': {
+                            'format': "%Y-%m-%d", 'date': "$feed_history_time"}}]}
+                    ]}}},
+                    {"$project": {
+                        "_id": 1,
+                        "feed_type_id": 1,
+                        "feed_dose": 1,
+                        "feed_history_time": 1,
+
+                    }}
+                ],
+                'as': 'feed_historys_today'
+            }},
+            {"$project": {
+                "_id": 1,
+                "id_int": 1,
+                "alias": 1,
+                "location": 1,
+                "feed_historys_today": '$feed_historys_today',
+                "total_feed_today": {"$sum": "$feed_historys_today.feed_dose"}
+            }}
+        ]
+        ponds = Pond.objects().aggregate(pipline)
+        response = []
+        for pond in ponds:
+            response.append(pond)
+        response = json.dumps(response, default=str)
         return Response(response, mimetype="application/json", status=200)
