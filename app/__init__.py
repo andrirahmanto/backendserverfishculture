833b6789e39683ea6cd5c29a5cf99752bfe1d986
Andri Rahmanto <rahmanto660@gmail.com>
2022-05-17 00:06:23 +0700

    feat: add view today feedhistory

--- app/__init__.py
+++ app/__init__.py
@@ -54,4 +54,91 @@ def create_app(test_config=None):
                 "%a || %d-%B-%Y || %H.%M.%f")
         return render_template('home.html', name='Andri', feedhistorys=enumerate(response, start=1))
 
+    @app.route('/feedhistory/today')
+    def feedhistoryToday():
+        # make variable for default field
+        date = datetime.today().strftime('%Y-%m-%d')
+        date = "2022-05-13"
+        print(date)
+        pipline = [
+            {'$lookup': {
+                'from': 'feed_history',
+                'let': {"pondid": "$_id"},
+                'pipeline': [
+                    {'$match': {'$expr': {'$and': [
+                        {'$eq': ['$pond_id', '$$pondid']},
+                        {'$eq': [date, {'$dateToString': {
+                            'format': "%Y-%m-%d", 'date': "$feed_history_time"}}]}
+                    ]}}},
+                    {'$lookup': {
+                        'from': 'feed_type',
+                        'let': {"feedid": "$feed_type_id"},
+                        'pipeline': [
+                            {'$match': {
+                                '$expr': {'$eq': ['$_id', '$$feedid']}}},
+                            {"$project": {
+                                "created_at": 0,
+                                "updated_at": 0,
+                            }}
+                        ],
+                        'as': 'feed_type'
+                    }},
+                    {"$project": {
+                        "_id": 1,
+                        "feed_type_id": 1,
+                        "feed_dose": 1,
+                        "feed_history_time": {'$dateToString': {
+                            'format': "%H:%M:%S", 'date': "$feed_history_time"}},
+                        "feed_type": 1,
+
+                    }}
+                ],
+                'as': 'feed_historys_today'
+            }},
+            {"$project": {
+                "_id": 1,
+                "id_int": 1,
+                "alias": 1,
+                "location": 1,
+                "feed_historys_today": '$feed_historys_today',
+                "total_feed_dose_today": {"$sum": "$feed_historys_today.feed_dose"}
+            }}
+        ]
+        ponds = Pond.objects().aggregate(pipline)
+        response = list(ponds)
+        return render_template('today.html', name='Andri', ponds=enumerate(response, start=1))
+
+    @app.route('/feedhistory/month')
+    def feedhistoryMonth():
+        # make variable for default field
+        date = datetime.today().strftime('%Y-%m')
+        print(date)
+        pipline = [
+            {'$lookup': {
+                'from': 'feed_history',
+                'let': {"pondid": "$_id"},
+                'pipeline': [
+                    {'$match': {'$expr': {'$and': [
+                        {'$eq': ['$pond_id', '$$pondid']},
+                        {'$eq': [date, {'$dateToString': {
+                            'format': "%Y-%m", 'date': "$feed_history_time"}}]}
+                    ]}}},
+                ],
+                'as': 'feed_historys_today'
+            }},
+            {"$project": {
+                "_id": 1,
+                "id_int": 1,
+                "alias": 1,
+                "location": 1,
+                "total_feed_historys": {"$size": "$feed_historys_today"},
+                "total_feed_dose": {"$sum": "$feed_historys_today.feed_dose"},
+                "avg_feed_dose": {'$avg': "$feed_historys_today.feed_dose"}
+            }}
+        ]
+        ponds = Pond.objects().aggregate(pipline)
+        response = list(ponds)
+        print(response)
+        return render_template('month.html', name='Andri', ponds=enumerate(response, start=1))
+
     return app
