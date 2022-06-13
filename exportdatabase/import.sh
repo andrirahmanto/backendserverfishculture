c775cc85bd86f6aa4fa2e2ec8c4c271a07d008dc
Andri Rahmanto <rahmanto660@gmail.com>
2022-06-14 01:34:05 +0700

    refactor: add shortcut to import db

--- exportdatabase/import.sh
+++ exportdatabase/import.sh
@@ -1,4 +1,5 @@
 mongoimport --db fishapi --collection pond --type json --file /var/www/html/fishapi/exportdatabase/pond.json --jsonArray
+mongoimport --db fishapi --collection pond_image --type json --file /var/www/html/fishapi/exportdatabase/pond_image.json --jsonArray
 mongoimport --db fishapi --collection feed_type --type json --file /var/www/html/fishapi/exportdatabase/feed_type.json --jsonArray
 mongoimport --db fishapi --collection feed_history --type json --file /var/www/html/fishapi/exportdatabase/feed_history.json --jsonArray
 mongoimport --db fishapi --collection mongoengine.counters --type json --file /var/www/html/fishapi/exportdatabase/mongoengine.counters.json --jsonArray
