2bd348e7ba2c0565c5b1eca2387dc79467455bfc
Andri Rahmanto <rahmanto660@gmail.com>
2022-06-16 00:04:43 +0700

    fix : fix for better response handle RESTful pond

--- fishapi/resources/controller/pond.py
+++ fishapi/resources/controller/pond.py
@@ -10,49 +10,61 @@ import json
 
 class PondsApi(Resource):
     def get(self):
-        url = url_for('pondimageapidummy', _external=True)
-        pipeline = [
-            {"$addFields": {
-                "area": {"$cond": {
-                    "if": {"$eq": ["$shape", "persegi"]},
-                    "then": {"$multiply": ["$length", "$width"]},
-                    "else": {"$divide": [
-                        {"$multiply": [22, "$diameter", "$diameter"]},
-                        28
-                    ]},
+        try:
+            url = url_for('pondimageapidummy', _external=True)
+            pipeline = [
+                {"$addFields": {
+                    "area": {"$cond": {
+                        "if": {"$eq": ["$shape", "persegi"]},
+                        "then": {"$multiply": ["$length", "$width"]},
+                        "else": {"$divide": [
+                            {"$multiply": [22, "$diameter", "$diameter"]},
+                            28
+                        ]},
+                    }},
+                    "image_link":{"$concat": [url, "/", {"$toString": "$_id"}]}
                 }},
-                "image_link":{"$concat": [url, "/", {"$toString": "$_id"}]}
-            }},
-            {"$addFields": {
-                "volume": {"$multiply": ["$area", "$height"]}
-            }},
-            {"$project": {
-                "pond_id": 0,
-                "feed_type_id": 0,
-                "created_at": 0,
-                "updated_at": 0,
-            }}
-        ]
-        ponds = Pond.objects.aggregate(pipeline)
-        list_ponds = list(ponds)
-        response = json.dumps(list_ponds, default=str)
-        return Response(response, mimetype="application/json", status=200)
+                {"$addFields": {
+                    "volume": {"$multiply": ["$area", "$height"]}
+                }},
+                {"$project": {
+                    "pond_id": 0,
+                    "feed_type_id": 0,
+                    "created_at": 0,
+                    "updated_at": 0,
+                }}
+            ]
+            ponds = Pond.objects.aggregate(pipeline)
+            list_ponds = list(ponds)
+            response = json.dumps(list_ponds, default=str)
+            return Response(response, mimetype="application/json", status=200)
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
 
     def post(self):
-        body = {
-            "alias": request.form.get("alias", None),
-            "location": request.form.get("location", None),
-            "shape": request.form.get("shape", None),
-            "material": request.form.get("material", None),
-            "length": request.form.get("length", None),
-            "width": request.form.get("width", None),
-            "diameter": request.form.get("diameter", None),
-            "height": request.form.get("height", None),
-            "build_at": request.form.get("build_at", None),
-        }
-        pond = Pond(**body).save()
-        id = pond.id
-        return {'id': str(id)}, 200
+        try:
+            body = {
+                "alias": request.form.get("alias", None),
+                "location": request.form.get("location", None),
+                "shape": request.form.get("shape", None),
+                "material": request.form.get("material", None),
+                "length": request.form.get("length", None),
+                "width": request.form.get("width", None),
+                "diameter": request.form.get("diameter", None),
+                "height": request.form.get("height", None),
+                "build_at": request.form.get("build_at", None),
+            }
+            pond = Pond(**body).save()
+            id = pond.id
+            response = {"message": "success add pond", "id": id}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=200)
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
 
 
 class PondImageApiDummy(Resource):
@@ -72,8 +84,16 @@ class PondImageApi(Resource):
         return send_from_directory(path, pond['image_name'])
 
     def put(self, id):
-        file = request.files['image']
-        if file and allowed_file(file.filename):
+        try:
+            file = request.files['image']
+            if not file:
+                response = {"message": "no file selected"}
+                response = json.dumps(response, default=str)
+                return Response(response, mimetype="application/json", status=400)
+            if not allowed_file(file.filename):
+                response = {"message": "file type not allowed"}
+                response = json.dumps(response, default=str)
+                return Response(response, mimetype="application/json", status=400)
             filename = secure_filename(file.filename)
             filename = pad_timestamp(filename)
             path = os.path.join(current_app.instance_path,
@@ -96,33 +116,50 @@ class PondImageApi(Resource):
             }
             objects.update(**data)
             id = objects.id
-            return {'id': str(id)}, 200
-        return {'error': 'tidak ada file'}, 400
-
-        # response = {"message": str(e)}
-        # response = json.dumps(response, default=str)
-        # return Response(response, mimetype="application/json", status=400)
+            response = {"message": "success change image", "id": id}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=200)
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
 
 
 class PondApi(Resource):
     def put(self, id):
-        body = {
-            "alias": request.form.get("alias", None),
-            "location": request.form.get("location", None),
-            "updated_at": datetime.datetime.utcnow()
-        }
-        Pond.objects.get(id=id).update(**body)
-        return '', 200
+        try:
+            body = {
+                "alias": request.form.get("alias", None),
+                "location": request.form.get("location", None),
+                "updated_at": datetime.datetime.utcnow()
+            }
+            Pond.objects.get(id=id).update(**body)
+            response = {"message": "success change data pond", "id": id}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=200)
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
 
     def delete(self, id):
-        pond = Pond.objects.get(id=id).delete()
-        return '', 200
+        try:
+            pond = Pond.objects.get(id=id).delete()
+            response = {"message": "success delete pond"}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=200)
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
 
     def get(self, id):
-        # init object pond
-        objects = Pond.objects.get(id=id)
-        # convert to dict
-        pond = objects.to_mongo()
-        # dump dict to json string
-        response_dump = json.dumps(pond, default=str)
-        return Response(response_dump, mimetype="application/json", status=200)
+        try:
+            objects = Pond.objects.get(id=id)
+            pond = objects.to_mongo()
+            response_dump = json.dumps(pond, default=str)
+            return Response(response_dump, mimetype="application/json", status=200)
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
