ee420d9570d0a08056646f94c3d10834e3ba1364
Andri Rahmanto <rahmanto660@gmail.com>
2022-07-27 17:06:04 +0700

    feat: add logic controller for fish grading

---
+++ fishapi/resources/controller/fishgrading.py
@@ -0,0 +1,167 @@
+from flask import Response, request
+from fishapi.database.models import FishGrading, Pond, PondActivation
+from flask_restful import Resource
+import datetime
+import json
+
+
+class FishGradingsApi(Resource):
+    def get(self):
+        try:
+            pipeline = [
+                {'$lookup': {
+                    'from': 'pond',
+                    'let': {"pondid": "$pond_id"},
+                    'pipeline': [
+                        {'$match': {'$expr': {'$eq': ['$_id', '$$pondid']}}},
+                        {"$project": {
+                            "_id": 1,
+                            "alias": 1,
+                            "location": 1,
+                            "build_at": 1,
+                            "isActive": 1,
+                        }}
+                    ],
+                    'as': 'pond'
+                }},
+                {'$lookup': {
+                    'from': 'pond_activation',
+                    'let': {"activationid": "$pond_activation_id"},
+                    'pipeline': [
+                        {'$match': {
+                            '$expr': {'$eq': ['$_id', '$$activationid']}}},
+                        {"$project": {
+                            "_id": 1,
+                            "isFinish": 1,
+                            "isWaterPreparation": 1,
+                            "water_level": 1,
+                            "activated_at": 1
+                        }}
+                    ],
+                    'as': 'pond_activation'
+                }},
+                {"$addFields": {
+                    "pond": {"$first": "$pond"},
+                    "pond_activation": {"$first": "$pond_activation"},
+                }},
+                {"$project": {
+                    "updated_at": 0,
+                    "created_at": 0,
+                }}
+            ]
+            fishgrading = FishGrading.objects.aggregate(pipeline)
+            list_fishgradings = list(fishgrading)
+            response = json.dumps(list_fishgradings, default=str)
+            return Response(response, mimetype="application/json", status=200)
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
+
+    def post(self):
+        try:
+            pond_id = request.form.get("pond_id", None)
+            pond = Pond.objects.get(id=pond_id)
+            if pond['isActive'] == False:
+                response = {"message": "pond is not active"}
+                response = json.dumps(response, default=str)
+                return Response(response, mimetype="application/json", status=400)
+            pond_activation = PondActivation.objects(
+                pond_id=pond_id, isFinish=False).order_by('-activated_at').first()
+            body = {
+                "pond_id": pond.id,
+                "pond_activation_id": pond_activation.id,
+                "fish_type": request.form.get("fish_type", None),
+                "sampling_amount": request.form.get("sampling_amount", None),
+                "avg_fish_weight": request.form.get("avg_fish_weight", None),
+                "avg_fish_long": request.form.get("avg_fish_long", None),
+                "amount_normal_fish": request.form.get("amount_normal_fish", None),
+                "amount_oversize_fish": request.form.get("amount_oversize_fish", None),
+                "amount_undersize_fish": request.form.get("amount_undersize_fish", None)
+            }
+            fishgrading = FishGrading(**body).save()
+            id = fishgrading.id
+            return {'id': str(id)}, 200
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
+
+
+class FishGradingApi(Resource):
+    def put(self, id):
+        try:
+            body = request.form.to_dict(flat=True)
+            FishGrading.objects.get(id=id).update(**body)
+            response = {
+                "message": "success change data fish grading", "id": id}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=200)
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
+
+    def delete(self, id):
+        try:
+            fishgrading = FishGrading.objects.get(id=id).delete()
+            response = {"message": "success delete fish grading"}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=200)
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
+
+    def get(self, id):
+        try:
+            pipeline = [
+                {'$match': {'$expr': {'$eq': ['$_id', {'$toObjectId': id}]}}},
+                {'$lookup': {
+                    'from': 'pond',
+                    'let': {"pondid": "$pond_id"},
+                    'pipeline': [
+                        {'$match': {'$expr': {'$eq': ['$_id', '$$pondid']}}},
+                        {"$project": {
+                            "_id": 1,
+                            "alias": 1,
+                            "location": 1,
+                            "build_at": 1,
+                            "isActive": 1,
+                        }}
+                    ],
+                    'as': 'pond'
+                }},
+                {'$lookup': {
+                    'from': 'pond_activation',
+                    'let': {"activationid": "$pond_activation_id"},
+                    'pipeline': [
+                        {'$match': {
+                            '$expr': {'$eq': ['$_id', '$$activationid']}}},
+                        {"$project": {
+                            "_id": 1,
+                            "isFinish": 1,
+                            "isWaterPreparation": 1,
+                            "water_level": 1,
+                            "activated_at": 1
+                        }}
+                    ],
+                    'as': 'pond_activation'
+                }},
+                {"$addFields": {
+                    "pond": {"$first": "$pond"},
+                    "pond_activation": {"$first": "$pond_activation"},
+                }},
+                {"$project": {
+                    "updated_at": 0,
+                    "created_at": 0,
+                }}
+            ]
+            fishgrading = FishGrading.objects.aggregate(pipeline)
+            list_fishgradings = list(fishgrading)
+            response = json.dumps(list_fishgradings[0], default=str)
+            return Response(response, mimetype="application/json", status=200)
+        except Exception as e:
+            response = {"message": str(e)}
+            response = json.dumps(response, default=str)
+            return Response(response, mimetype="application/json", status=400)
