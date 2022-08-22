250a36dc1e3fbe00b2551e93f2f9348a4f4a3adc
Andri Rahmanto <rahmanto660@gmail.com>
2022-08-22 22:55:53 +0700

    feat: add new chart.js

--- fishapi/static/js/custom_page/fishgrading/monthly.js
+++ fishapi/static/js/custom_page/fishgrading/monthly.js
@@ -7,64 +7,102 @@
 
 "use strict";
 
-// var ctx = document.getElementById("myChart").getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
-//         datasets: [{
-//             label: 'Lele',
-//             data: [1, 1.5, 2.1, 2.3, 2.6, 3.1, 4, 4.2],
-//             borderWidth: 2,
-//             backgroundColor: 'rgba(63,82,227,.8)',
-//             borderWidth: 2,
-//             borderColor: 'rgba(63,82,227,.8)',
-//             pointBorderWidth: 0,
-//             pointRadius: 3.5,
-//             pointBackgroundColor: 'rgba(63,82,227,.8)',
-//             pointHoverBackgroundColor: 'rgba(63,82,227,.8)',
-//         },
-//         {
-//             label: 'Mas',
-//             data: [0.4, 1.1, 2, 2.1, 3.2, 3.4, 4, 4.3],
-//             borderWidth: 2,
-//             backgroundColor: 'rgba(254,86,83,.7)',
-//             borderWidth: 2,
-//             borderColor: 'rgba(254,86,83,.7)',
-//             pointBorderWidth: 0,
-//             pointRadius: 3.5,
-//             pointBackgroundColor: 'rgba(254,86,83,.7)',
-//             pointHoverBackgroundColor: 'rgba(254,86,83,.8)',
-//         }]
-//     },
-//     options: {
-//         legend: {
-//             display: false
-//         },
-//         scales: {
-//             yAxes: [{
-//                 gridLines: {
-//                     // display: false,
-//                     drawBorder: false,
-//                     color: '#f2f2f2',
-//                 },
-//                 ticks: {
-//                     beginAtZero: true,
-//                     stepSize: 1500,
-//                     callback: function (value, index, values) {
-//                         return '$' + value;
-//                     }
-//                 }
-//             }],
-//             xAxes: [{
-//                 gridLines: {
-//                     display: false,
-//                     tickMarkLength: 15,
-//                 }
-//             }]
-//         },
-//     }
-// });
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
+        datasets: [
+            {
+                label: 'Nila Merah',
+                data: [0.4, 1.1, 2, 2.1, 3.2, 3.4, 4, 4.3],
+                borderWidth: 2,
+                borderWidth: 2,
+                backgroundColor: 'transparent',
+                borderColor: 'rgba(254,86,83,.7)',
+                pointBorderWidth: 0,
+                pointRadius: 3.5,
+                pointBackgroundColor: 'rgba(254,86,83,.7)',
+                pointHoverBackgroundColor: 'rgba(254,86,83,.8)',
+            },
+            {
+                label: 'Nila Hitam',
+                data: [0.4, 0.6, 3, 3.1, 3.8, 4, 4.3, 4.6],
+                borderWidth: 2,
+                borderWidth: 2,
+                backgroundColor: 'transparent',
+                borderColor: 'rgba(0, 0, 0, 1)',
+                pointBorderWidth: 0,
+                pointRadius: 3.5,
+                pointBackgroundColor: 'rgba(0, 0, 0, 1)',
+                pointHoverBackgroundColor: 'rgba(0, 0, 0, 1)',
+            },
+            {
+                label: 'Lele',
+                data: [1, 1.5, 2.1, 2.3, 2.6, 3.1, 4, 4.2],
+                borderWidth: 2,
+                borderWidth: 2,
+                backgroundColor: 'transparent',
+                borderColor: 'rgba(63,82,227,.8)',
+                pointBorderWidth: 0,
+                pointRadius: 3.5,
+                pointBackgroundColor: 'rgba(63,82,227,.8)',
+                pointHoverBackgroundColor: 'rgba(63,82,227,.8)',
+            },
+            {
+                label: 'Mas',
+                data: [0.1, 0.4, 0.8, 1.2, 3, 4, 5, 5.5],
+                borderWidth: 2,
+                borderWidth: 2,
+                backgroundColor: 'transparent',
+                borderColor: 'rgba(229, 249, 9, 1)',
+                pointBorderWidth: 0,
+                pointRadius: 3.5,
+                pointBackgroundColor: 'rgba(229, 249, 9, 1)',
+                pointHoverBackgroundColor: 'rgba(229, 249, 9, 1)',
+            },
+            {
+                label: 'Patin',
+                data: [0, 0, 0, 0, 0, 0, 0, 0],
+                borderWidth: 2,
+                borderWidth: 2,
+                backgroundColor: 'transparent',
+                borderColor: 'rgba(25, 250, 10, 0.74)',
+                pointBorderWidth: 0,
+                pointRadius: 3.5,
+                pointBackgroundColor: 'rgba(25, 250, 10, 0.74)',
+                pointHoverBackgroundColor: 'rgba(25, 250, 10, 0.74)',
+            },
+        ]
+    },
+    options: {
+        legend: {
+            display: true
+        },
+        scales: {
+            yAxes: [{
+                gridLines: {
+                    // display: false,
+                    drawBorder: true,
+                    color: '#f2f2f2',
+                },
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 1,
+                    callback: function (value, index, values) {
+                        return value + 'Kg';
+                    }
+                }
+            }],
+            xAxes: [{
+                gridLines: {
+                    display: false,
+                    tickMarkLength: 15,
+                }
+            }]
+        },
+    }
+});
 
 
 $(document).ready(function () {
